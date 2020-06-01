907d7b530b7e00ce8a938f76b4e42d30f8e74727
LouisPoidevin <21906175@etu.unicaen.fr>
2020-06-02 00:00:53 +0200

    tp3 2.2

--- tp3/app/controllers/Organizations.php
+++ tp3/app/controllers/Organizations.php
@@ -16,4 +16,12 @@ class Organizations extends \Ubiquity\controllers\ControllerBase{
         $organizations=DAO::getAll(Organization::class);
         $this->loadView("Organizations/index.html",["orgas"=>$organizations]);
     }
+
+    /**
+        * @get("{idOrga}","name"=>"orgas-display")
+        **/    
+        public function display($idOrga){
+            $orga=DAO::getById(Organization::class, $idOrga, ['users','groupes']);
+            $this->loadView("Organizations/display.html",["orga"=>$orga]);
+        }
 }
