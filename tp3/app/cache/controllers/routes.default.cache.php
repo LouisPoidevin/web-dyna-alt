12fd78199012bad48761f7d8bff8888ba87e31dd
LouisPoidevin <21906175@etu.unicaen.fr>
2020-06-01 23:39:58 +0200

    tp3 2.1

--- tp3/app/cache/controllers/routes.default.cache.php
+++ tp3/app/cache/controllers/routes.default.cache.php
@@ -1,2 +1,2 @@
 <?php
-return array();
+return array("/organizations/(index/)?"=>array("get"=>array("controller"=>"controllers\\Organizations","action"=>"index","parameters"=>array(),"name"=>"orgas-index","cache"=>false,"duration"=>false)));
---
+++ tp3/app/controllers/Organizations.php
@@ -0,0 +1,19 @@
+<?php
+namespace controllers;
+ use Ubiquity\orm\DAO;
+ use models\Organization;
+ 
+ /**
+ * Controller Organizations
+ * @route("organizations")
+ **/
+class Organizations extends \Ubiquity\controllers\ControllerBase{
+ 
+        /**
+        * @get("name"=>"orgas-index")
+        **/
+    public function index(){
+        $organizations=DAO::getAll(Organization::class);
+        $this->loadView("Organizations/index.html",["orgas"=>$organizations]);
+    }
+}
