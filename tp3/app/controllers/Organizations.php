9c97fb3773d6db8e6ace6a7667af5f3cdf74e6bd
LouisPoidevin <21906175@etu.unicaen.fr>
2020-06-02 06:11:39 +0200

    tp3 2.3

--- tp3/app/controllers/Organizations.php
+++ tp3/app/controllers/Organizations.php
@@ -8,20 +8,27 @@ namespace controllers;
  * @route("organizations")
  **/
 class Organizations extends \Ubiquity\controllers\ControllerBase{
- 
-        /**
-        * @get("name"=>"orgas-index")
-        **/
+
+    protected function users($users=null){
+        $title="Tous les utilisateurs";
+        return $this->loadView("Organizations/users.html",compact("users","title"),true);
+    }
+
+    /**
+     * @get("name"=>"orgas-index")
+     **/
     public function index(){
         $organizations=DAO::getAll(Organization::class);
         $this->loadView("Organizations/index.html",["orgas"=>$organizations]);
     }
 
     /**
-        * @get("{idOrga}","name"=>"orgas-display")
-        **/    
-        public function display($idOrga){
-            $orga=DAO::getById(Organization::class, $idOrga, ['users','groupes']);
-            $this->loadView("Organizations/display.html",["orga"=>$orga]);
-        }
+     * @get("{idOrga}","name"=>"orgas-display")
+     **/    
+    public function display($idOrga){
+        $orga=DAO::getById(Organization::class, $idOrga,['users','groupes']);
+        $users=$this->users($orga->getUsers());
+        $this->loadView("Organizations/display.html",["orga"=>$orga,"users"=>$users]);
+    }
+
 }
